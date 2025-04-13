b0dfe3126dd8e89a559dba38298929367b044db5
Pepengu <plotnikovdanya@gmail.com>
2025-04-14 01:21:37 +0300

    fix: docker works without sudo

--- NixOS/nixos-modules/desktop/users.nix
+++ NixOS/nixos-modules/desktop/users.nix
@@ -1,7 +1,13 @@
-_: {
+{pkgs, ...}: {
+
+  security.sudo.wheelNeedsPassword = false;
+  nix.settings.trusted-users = ["root" "@wheel"];
+
     users.users.daniil = {
         isNormalUser = true;
         description = "Daniil";
+#        home = ~/home/daniil;
+#        shell = pkgs.fish;
         extraGroups = [ "networkmanager" "wheel" "docker"];
     };
 }
